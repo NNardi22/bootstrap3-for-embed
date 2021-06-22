19a48591b38c6aeb1405f28f35fb4b597d618f38
NNardi22 <nnardi@simplicar.com>
2021-06-22 17:01:59 -0300

    making the structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="container">
-    <h1>Projecto con bootstrap 3</h1>
+    <h1>Proyecto con bootstrap 3</h1>
+
+    <div class="row my-3">
+      <div class="col-md-6 themed-cols">
+        <h3>Componentes de bootstrap 3</h3>
+      </div>
+      <div class="col-md-6 themed-cols">
+        <h3>Sitio embebido con bootstrap 5</h3>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.themed-cols {
+  padding-top: 15px;
+  padding-bottom: 15px;
+  background-color: rgba(86, 61, 124, 0.15);
+  border: 1px solid rgba(86, 61, 124, 0.2);
+}
+</style>
 
 <script>
 export default {};
